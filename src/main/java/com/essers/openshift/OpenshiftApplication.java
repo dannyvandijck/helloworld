047b1eca7286ac1129291be2253c79a7f76a197e
dannyvandijck <phan81@hotmail.com>
2020-10-19 11:28:56 +0200

    Add files via upload

---
+++ src/main/java/com/essers/openshift/OpenshiftApplication.java
@@ -0,0 +1,27 @@
+package com.essers.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OpenshiftApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello world";
+	}
+
+	@GetMapping("/hello/{user}")
+	public String helloMe(@PathVariable String user) {
+		return "Hello " + user;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftApplication.class, args);
+	}
+
+}
